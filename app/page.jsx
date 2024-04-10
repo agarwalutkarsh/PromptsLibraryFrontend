a4e0777fe38b68ec5b1f96d3c4186f31a6a70e6d
Utkarsh Agarwal <utkarsh.agarwal2@insight.com>
2024-04-10 16:26:23 +0530

    test css

--- app/page.jsx
+++ app/page.jsx
@@ -7,9 +7,10 @@ const Home = () => {
     // Home Page
     <>
       <div className="flex flex-col text-center w-full">
-        <Typography component='h1' className="head_text">Discover & Share</Typography>
+        <Typography component='h1' className="mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl">Discover & Share</Typography>
         <br className="max-md:hidden" />
-        <Typography className="orange_gradient head_text">AI Powered Prompts</Typography>
+        <Typography className="orange_gradient mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl">AI Powered Prompts</Typography>
+        <p className="mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, cupiditate, ducimus error cumque commodi rerum, odio voluptatibus exercitationem ipsam maiores officia voluptates quidem. Doloribus recusandae itaque molestias, tempora accusantium aliquid?</p>
         <Typography className="desc m-auto">Open Source Tool for the mordern world to discover, create and share creative AI powered prompts</Typography>
       </div>
 
